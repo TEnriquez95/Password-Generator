b22f7bc00317e7cd3421ac7898a972e7fecf1443
Princess <princesstheaenriquez@gmail.com>
2021-04-11 23:04:17 -0500

    Attempted to connect button to prompts.

--- Develop/script.js
+++ Develop/script.js
@@ -1,39 +1,44 @@
 // Assignment code here
 //DOM Elements
-const generateBtn = document.getElementById('generate');
+const generateBtn = document.getElementById("generate");
+const passwordDom = document.getElementById("password");
+// Get references to the #generate element
 
+var generateBtn = document.querySelector("#generate");
 
-
+//charCodes for password generation
 const upperCase = arrayFromLowtoHigh(65, 90);
 const lowerCase = arrayFromLowtoHigh(97, 122);
 const numberCodes = arrayFromLowtoHigh(48, 57);
 const specialCodes = arrayFromLowtoHigh(33, 47)
-      .concat(arrayFromLowtoHigh(58, 64))
-      .concat(arrayFromLowtoHigh(91, 96))
-      .concat(arrayFromLowtoHigh(123, 126));
-// Get references to the #generate element
-
-var generateBtn = document.querySelector("#generate");
+  .concat(arrayFromLowtoHigh(58, 64))
+  .concat(arrayFromLowtoHigh(91, 96))
+  .concat(arrayFromLowtoHigh(123, 126));
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+}
+function generatePassword() {
+  var passwordLength = prompt("How many characters do you want for your new password? Please enter a number between 8 and 128.");
+  var lowerCaseSelect = confirm("Would you like it to include lowercase characters?");
+  var upperCaseSelect = confirm("Would you like it to include uppercase characters?");
+  var numericSelect = confirm("Would you like it to include numeric values?");
+  var specialSelect = confirm("Would you like it to include special characters?");
 }
 
 //Prompts after clicking Generate Password
-var passwordLength = prompt("How many characters do you want for your new password? Please enter a number between 8 and 128.");
-var lowerCaseSelect = confirm("Would you like it to include lowercase characters?");
-var upperCaseSelect = confirm("Would you like it to include uppercase characters?");
-var numericSelect = confirm("Would you like it to include numeric values?")
-var specialSelect = confirm("Would you like it to include special characters?")
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", function () {
+  ps = generatePassword();
+  document.getElementById("password").placeholder = ps;
+});
+
 
 
 //Password generating function
